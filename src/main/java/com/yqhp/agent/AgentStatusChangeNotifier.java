cacaac59fc59663f060fd500dbb9e03ab9f06386
jiangyitao <451988022@163.com>
2019-04-10 00:32:22 +0800

    update

---
+++ src/main/java/com/yqhp/agent/AgentStatusChangeNotifier.java
@@ -0,0 +1,58 @@
+package com.yqhp.agent;
+
+import com.yqhp.mbg.mapper.DeviceMapper;
+import com.yqhp.mbg.po.Device;
+import com.yqhp.mbg.po.DeviceExample;
+import de.codecentric.boot.admin.server.domain.entities.Instance;
+import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
+import de.codecentric.boot.admin.server.domain.events.InstanceEvent;
+import de.codecentric.boot.admin.server.domain.events.InstanceStatusChangedEvent;
+import de.codecentric.boot.admin.server.domain.values.StatusInfo;
+import de.codecentric.boot.admin.server.notify.AbstractStatusChangeNotifier;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * Created by jiangyitao.
+ * Agent状态变化通知器
+ */
+@Component
+@Slf4j
+public class AgentStatusChangeNotifier extends AbstractStatusChangeNotifier {
+
+    @Autowired
+    private DeviceMapper deviceMapper;
+
+    public AgentStatusChangeNotifier(InstanceRepository repositpry) {
+        super(repositpry);
+    }
+
+    @Override
+    protected Mono<Void> doNotify(InstanceEvent event, Instance instance) {
+        return Mono.fromRunnable(() -> {
+            if (event instanceof InstanceStatusChangedEvent) {
+                String status = ((InstanceStatusChangedEvent) event).getStatusInfo().getStatus();
+                if (!StatusInfo.STATUS_UP.equals(status)) {
+                    //非在线
+                    String agentURL = instance.getRegistration().getServiceUrl();// http://xx.xx.xx.xx:xxx/
+                    log.info("检测到agent {} 处于非在线状态", agentURL);
+                    String agentIp = agentURL.split("//")[1].split(":")[0];// xx.xx.xx.xx
+
+                    //把该agent下的设备都改为离线
+                    Device device = new Device();
+                    device.setStatus(Device.OFFLINE_STATUS);
+                    device.setLastOfflineTime(new Date());
+
+                    DeviceExample deviceExample = new DeviceExample();
+                    deviceExample.createCriteria().andAgentIpEqualTo(agentIp);
+
+                    deviceMapper.updateByExampleSelective(device, deviceExample);
+                }
+            }
+        });
+    }
+}
--- src/test/java/test/Test1.java
+++ src/test/java/test/Test1.java
@@ -1,10 +1,18 @@
 package test;
 
 import com.yqhp.Application;
+import com.yqhp.mbg.po.Device;
+import com.yqhp.model.Response;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by jiangyitao.
@@ -13,7 +21,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = Application.class)
 public class Test1 {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Test
     public void test() {
+        Map map = new HashMap();
     }
 }
