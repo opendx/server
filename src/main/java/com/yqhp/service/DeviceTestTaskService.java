a22125a4aca6705c6bb539ca005abdff92cd3aa4
jiangyitao <451988022@163.com>
2019-05-04 23:35:43 +0800

    update

--- src/main/java/com/yqhp/service/DeviceTestTaskService.java
+++ src/main/java/com/yqhp/service/DeviceTestTaskService.java
@@ -3,17 +3,21 @@ package com.yqhp.service;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.yqhp.mbg.mapper.DeviceTestTaskMapper;
+import com.yqhp.mbg.mapper.TestTaskMapper;
 import com.yqhp.mbg.po.DeviceTestTask;
 import com.yqhp.mbg.po.DeviceTestTaskExample;
+import com.yqhp.mbg.po.TestTask;
 import com.yqhp.model.Page;
 import com.yqhp.model.PageRequest;
 import com.yqhp.model.Response;
+import com.yqhp.model.vo.Testcase;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -25,18 +29,54 @@ public class DeviceTestTaskService {
 
     @Autowired
     private DeviceTestTaskMapper deviceTestTaskMapper;
+    @Autowired
+    private TestTaskMapper testTaskMapper;
 
     public Response update(DeviceTestTask deviceTestTask) {
         if (deviceTestTask.getId() == null) {
             return Response.fail("testTaskDevice不能为空");
         }
 
-        int insertRow = deviceTestTaskMapper.insertSelective(deviceTestTask);
+        int insertRow = deviceTestTaskMapper.updateByPrimaryKeySelective(deviceTestTask);
         if (insertRow != 1) {
             return Response.fail("更新失败，请稍后重试");
-        } else {
-            return Response.success("更新成功");
         }
+
+        if(deviceTestTask.getStatus() != null && deviceTestTask.getStatus() == DeviceTestTask.FINISHED_STATUS) {
+            deviceTestTask = deviceTestTaskMapper.selectByPrimaryKey(deviceTestTask.getId());
+            DeviceTestTaskExample deviceTestTaskExample = new DeviceTestTaskExample();
+            deviceTestTaskExample.createCriteria().andTestTaskIdEqualTo(deviceTestTask.getTestTaskId());
+            List<DeviceTestTask> deviceTestTasks = deviceTestTaskMapper.selectByExampleWithBLOBs(deviceTestTaskExample);
+
+            long unFinishedTask = deviceTestTasks.stream().filter(task -> task.getStatus() != DeviceTestTask.FINISHED_STATUS).count();
+            if(unFinishedTask == 0) { // 本次测试，所有设备都完成了
+                // 统计测试结果数据
+                int passCount = 0;
+                int failCount = 0;
+                int skipCount = 0;
+                for(DeviceTestTask testTask: deviceTestTasks) {
+                    for(Testcase testcase: testTask.getTestcases()) {
+                        if(testcase.getStatus() == Testcase.PASS_STATUS) {
+                            passCount = passCount + 1;
+                        }else if(testcase.getStatus() == Testcase.FAIL_STATUS) {
+                            failCount = failCount + 1;
+                        }else if(testcase.getStatus() == Testcase.SKIP_STATUS) {
+                            skipCount = skipCount + 1;
+                        }
+                    }
+                }
+                TestTask testTask = new TestTask();
+                testTask.setId(deviceTestTask.getTestTaskId());
+                testTask.setStatus(TestTask.FINISHED_STATUS);
+                testTask.setFinishTime(new Date());
+                testTask.setPassCaseCount(passCount);
+                testTask.setFailCaseCount(failCount);
+                testTask.setSkipCaseCount(skipCount);
+                testTaskMapper.updateByPrimaryKeySelective(testTask);
+            }
+        }
+
+        return Response.success("更新成功");
     }
 
     public Response list(DeviceTestTask deviceTestTask, PageRequest pageRequest) {
--- src/test/java/test/Test1.java
+++ src/test/java/test/Test1.java
@@ -1,18 +1,28 @@
 package test;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.yqhp.Application;
 import com.yqhp.mbg.po.Device;
 import com.yqhp.model.Response;
+import com.yqhp.model.vo.AgentVo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
+import java.io.File;
 import java.util.List;
 import java.util.Map;
+
 
 /**
  * Created by jiangyitao.
@@ -26,6 +36,19 @@ public class Test1 {
 
     @Test
     public void test() {
-        Map map = new HashMap();
+
+
+        String filePath = "C:\\Users\\jyt\\Desktop\\微信图片_20190412224937.png";
+
+        FileSystemResource resource = new FileSystemResource(new File(filePath));
+        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        params.add("file", resource);
+
+
+        Response response = restTemplate.postForObject("http://localhost:8887/upload/file", params, Response.class);
+
+        Map<String,String> map = JSON.parseObject(JSON.toJSONString(response.getData()), Map.class);
+
+        System.out.println(map.get("downloadURL"));
     }
 }
