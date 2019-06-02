825bf08f350a4d60f5a1e561390b2ca7319a4489
jiangyitao <451988022@163.com>
2019-06-02 22:28:51 +0800

    update

--- src/main/java/com/fgnb/config/CorsConfigurer.java
+++ src/main/java/com/fgnb/config/CorsConfigurer.java
@@ -11,9 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfigurer implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/**")
-                .allowedOrigins("*")
-                .allowCredentials(true);
+        registry.addMapping("/**").allowedOrigins("*").allowCredentials(true);
     }
 }
