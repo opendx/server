7262bf3b1542b25fa345557396feef317672c648
jiangyitao <451988022@163.com>
2019-06-08 01:07:44 +0800

    update

--- src/main/java/com/fgnb/service/ActionService.java
+++ src/main/java/com/fgnb/service/ActionService.java
@@ -1,7 +1,11 @@
 package com.fgnb.service;
 
 import com.alibaba.fastjson.JSONObject;
+import com.fgnb.exception.BusinessException;
 import com.fgnb.model.UserCache;
+import com.fgnb.model.action.LocalVar;
+import com.fgnb.model.action.Param;
+import com.fgnb.model.action.Step;
 import com.github.pagehelper.PageHelper;
 import com.fgnb.agent.AgentApi;
 import com.fgnb.dao.ActionDao;
@@ -17,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -28,6 +33,11 @@ import java.util.stream.Collectors;
 @Slf4j
 public class ActionService extends BaseService {
 
+    private static final String GLOBAL_VAR_PREFIX = "${"; // 全局变量前缀
+    private static final String ACTION_PARAM_PREFIX = "#{"; // 方法参数前缀
+    private static final String LOCAL_VAR_PREFIX = "@{"; // 局部变量前缀
+    private static final String COMMON_SUFFIX = "}";
+
     @Autowired
     private ActionMapper actionMapper;
     @Autowired
@@ -38,10 +48,11 @@ public class ActionService extends BaseService {
     private AgentApi agentApi;
 
     public Response add(Action action) {
+        check(action);
+
         action.setCreatorUid(getUid());
         action.setCreateTime(new Date());
 
-        // todo 校验是否符合规范 前端逻辑转到后端来
         int insertRow;
         try {
             insertRow = actionMapper.insertSelective(action);
@@ -88,10 +99,12 @@ public class ActionService extends BaseService {
             return Response.fail("actionId不能为空");
         }
 
+        check(action);
+
         action.setUpdateTime(new Date());
         action.setUpdatorUid(getUid());
 
-        // todo 校验
+        // 为了防止更新action影响到其他action，暂时在前端屏蔽了更新方法参数与返回值（即方法参数无法添加删除，返回值无法编辑）
         int updateRow;
         try {
             updateRow = actionMapper.updateByPrimaryKeyWithBLOBs(action);
@@ -134,6 +147,58 @@ public class ActionService extends BaseService {
             return Response.success(Page.build(actionVos, total));
         } else {
             return Response.success(actionVos);
+        }
+    }
+
+    /**
+     * 校验action是否符合规则，防止编译出错。部分已经用@Valid校验过
+     *
+     * @param action
+     */
+    private void check(Action action) {
+        // 1. 检查返回值。可以是一个普通的字符串或方法参数或局部变量或全局变量。全局变量暂时不检查
+        if (action.getHasReturnValue() == Action.HAS_RETURN_VALUE) {
+            String returnValue = action.getReturnValue(); // 前端已校验过，此时返回值一定有值
+            if (returnValue.startsWith(ACTION_PARAM_PREFIX) && returnValue.endsWith(COMMON_SUFFIX)) {
+                // 返回值 -> 方法参数
+                List<Param> params = action.getParams();
+                if (CollectionUtils.isEmpty(params)) {
+                    throw new BusinessException("返回值" + returnValue + "为方法参数，但方法参数为空");
+                }
+                long count = params.stream().filter(param -> returnValue.substring(2, returnValue.length() - 1) == param.getName()).count();
+                if (count != 1) {
+                    throw new BusinessException("返回值" + returnValue + "为方法参数，但与返回值匹配的方法参数的个数不为1");
+                }
+            } else if (returnValue.startsWith(LOCAL_VAR_PREFIX) && returnValue.endsWith(COMMON_SUFFIX)) {
+                // 返回值 -> 局部变量
+                List<LocalVar> localVars = action.getLocalVars();
+                if (CollectionUtils.isEmpty(localVars)) {
+                    throw new BusinessException("返回值" + returnValue + "为局部变量，但局部变量为空");
+                }
+                long count = localVars.stream().filter(localVar -> returnValue.substring(2, returnValue.length() - 1) == localVar.getName()).count();
+                if (count != 1) {
+                    throw new BusinessException("返回值" + returnValue + "为局部变量，但与返回值匹配的局部变量的个数不为1");
+                }
+            }
+        }
+
+        // 2. 检查赋值。赋值必须为局部变量，给方法参数或全部变量重新赋值，意义不大
+        List<Step> steps = action.getSteps(); // @valid已校验steps不为空
+        List<String> evaluations = steps.stream().filter(step -> !StringUtils.isEmpty(step.getEvaluation())).map(Step::getEvaluation).collect(Collectors.toList());
+        if (!CollectionUtils.isEmpty(evaluations)) {
+            List<LocalVar> localVars = action.getLocalVars();
+            if (CollectionUtils.isEmpty(localVars)) {
+                throw new BusinessException("赋值不为空，但局部变量为空");
+            }
+            for (String evaluation : evaluations) {
+                if (!evaluation.startsWith(LOCAL_VAR_PREFIX) || !evaluation.endsWith(COMMON_SUFFIX)) {
+                    throw new BusinessException(evaluation + "必须为局部变量");
+                }
+                long count = localVars.stream().filter(localVar -> evaluation.substring(2, evaluation.length() - 1) == localVar.getName()).count();
+                if (count != 1) {
+                    throw new BusinessException("与赋值" + evaluation + "匹配的局部变量的个数不为1");
+                }
+            }
         }
     }
 
@@ -267,7 +332,7 @@ public class ActionService extends BaseService {
     }
 
     public List<Action> findByProjectIdAndGlobalVar(Integer projectId, String globalVarName) {
-        return actionDao.selectByProjectIdAndGlobalVarName(projectId, "${" + globalVarName + "}");
+        return actionDao.selectByProjectIdAndGlobalVarName(projectId, GLOBAL_VAR_PREFIX + globalVarName + COMMON_SUFFIX);
     }
 
 }
