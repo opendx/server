75513f8210bc7ebad9eea0f5e470788423debbe7
jiangyitao <451988022@163.com>
2020-04-20 10:57:41 +0800

    0.5.5

--- db/update.sql
+++ db/update.sql
@@ -1,6 +1,8 @@
--- 0.5.3
+-- 0.5.5
 ALTER TABLE `page`
 MODIFY COLUMN `window_orientation` varchar(11) NULL COMMENT '屏幕方向' AFTER `window_width`;
 
 ALTER TABLE `page`
 ADD COLUMN `type` tinyint(4) NOT NULL COMMENT '1.android_native 2.ios_native 3.web' AFTER `project_id`;
+
+UPDATE page pg JOIN project pj ON pg.project_id = pj.id SET type = IF(pj.platform = 1, 1, 2)
