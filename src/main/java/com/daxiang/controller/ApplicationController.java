2e4b7601141410767a04633c86c2028e2a9d2f0b
jiangyitao <451988022@163.com>
2020-07-25 11:21:40 +0800

    app version

---
+++ src/main/java/com/daxiang/controller/ApplicationController.java
@@ -0,0 +1,24 @@
+package com.daxiang.controller;
+
+import com.daxiang.model.Response;
+import com.google.common.collect.ImmutableMap;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jiangyitao.
+ */
+@RestController
+@RequestMapping("/application")
+public class ApplicationController {
+
+    @Value("${version}")
+    private String version;
+
+    @GetMapping("/version")
+    public Response version() {
+        return Response.success(ImmutableMap.of("version", version));
+    }
+}
